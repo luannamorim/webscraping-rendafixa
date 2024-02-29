8f0f54a4b283406930e817bf859ce92c8bf2b345
luann <luanntech@gmail.com>
2024-02-29 00:58:12 -0400

    wb apprendafixa

--- apprendafixa.py
+++ apprendafixa.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
+import re
+import pandas as pd
+from datetime import datetime
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -16,33 +19,74 @@ url = 'https://apprendafixa.com.br/app/investimentos/rendafixa'
 
 driver.get(url)
 
-numero_paginas = WebDriverWait(driver, 3600).until(
+numero_paginas = WebDriverWait(driver, 30).until(
     EC.presence_of_element_located(
-        (By.CLASS_NAME, 'mat-paginator-range-label'))
-)
+        (By.CLASS_NAME, 'mat-paginator-range-label')))
 
 range_label_text = numero_paginas.text.strip()
 index = range_label_text.find('de ')
 qtd_itens = range_label_text[index + 3:]
 ultima_pagina = math.ceil(int(qtd_itens) / 60)
 
-elements = WebDriverWait(driver, 60).until(
-    EC.presence_of_all_elements_located(
-        (By.CLASS_NAME, 'card')
-    )
-)
+dic_produtos = {'produto': [], 'instituição': [], 'Valor Mínimo': [], 'Rentabilidade': [], 'Valor Líquido': [],
+                'Rating do Emissor': [], 'Vencimento': [], 'Liquidez': [], 'Taxa': [], 'Detalhes': []}
 
-for element in elements:
-    conteudo_elemento = element.text.strip()
-    print(conteudo_elemento)
+try:
+    for i in range(1, ultima_pagina+1):
+        elements = WebDriverWait(driver, 30).until(
+            EC.presence_of_all_elements_located(
+                (By.CLASS_NAME, 'card')
+            )
+        )
 
+        print("Página " + str(i))
 
-driver.quit()
+        for element in elements:
+            texto = element.text
+            palavras_remover = [
+                "Valor Mínimo",
+                "Rentabilidade",
+                "Valor Líquido",
+                "Rating do Emissor",
+                "Vencimento",
+                "Liquidez",
+                "Taxa",
+                "INVESTIR",
+                "+ DETALHES"
+            ]
 
+            padrao = "|".join(map(re.escape, palavras_remover))
+            texto_limpo = re.sub(padrao, "", texto)
+            linhas = texto_limpo.split('\n')
 
-# next_page_button = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable(
-#         (By.XPATH, "/html/body/app-root/app-main-page/mat-sidenav-container/mat-sidenav-content/div/app-rendafixa-page/div[2]/mat-paginator/div/div/div[2]/button[2]"))
-# )
+            dic_produtos['produto'].append(linhas[0])
+            dic_produtos['instituição'].append(linhas[1])
+            dic_produtos['Valor Mínimo'].append(linhas[3])
+            dic_produtos['Rentabilidade'].append(linhas[5])
+            dic_produtos['Valor Líquido'].append(linhas[7])
+            dic_produtos['Rating do Emissor'].append(linhas[9])
+            dic_produtos['Vencimento'].append(linhas[11])
+            dic_produtos['Liquidez'].append(linhas[13])
+            dic_produtos['Taxa'].append(linhas[15])
+            dic_produtos['Detalhes'].append(linhas[16])
 
-# driver.execute_script("arguments[0].click();", next_page_button)
+        next_page_button = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "/html/body/app-root/app-main-page/mat-sidenav-container/mat-sidenav-content/div/app-rendafixa-page/div[2]/mat-paginator/div/div/div[2]/button[2]"))
+        )
+
+        driver.execute_script("arguments[0].click();", next_page_button)
+
+except Exception as e:
+    print("Processo Finalizado")
+
+finally:
+
+    driver.quit()
+
+    data_atual = datetime.now()
+    nome_arquivo = data_atual.strftime("%Y-%m-%d") + ".csv"
+    caminho_arquivo = r'C:\Users\Luann\Downloads\\' + nome_arquivo
+    df = pd.DataFrame(dic_produtos)
+    df.to_csv(caminho_arquivo, encoding='utf-8-sig', sep=';', index=False)
+    print("Documento salvo em: " + caminho_arquivo)
--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-data_atual = datetime.now()
-nome_arquivo = data_atual.strftime("%Y-%m-%d") + ".csv"
-caminho_arquivo = r'C:\Users\Luann\Downloads\\' + nome_arquivo
-
-print(caminho_arquivo)
